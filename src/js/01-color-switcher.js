9989140250218ee64ee3d5597d92824f7648c2f9
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-09 15:50:38 +0300

    add Tack_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startBtn.addEventListener('click', onClickStartChangeColor);
+stopBtn.addEventListener('click', onClickStopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onClickStartChangeColor() {
+  changeColor = setInterval(() => {
+    startBtn.disabled = true;
+    bodyEl.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStopChangeColor() {
+  clearInterval(changeColor);
+  startBtn.disabled = false;
+}
