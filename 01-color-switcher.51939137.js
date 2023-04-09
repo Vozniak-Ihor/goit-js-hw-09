46fcdb55c3876ccae97c111b1d273eed7e7dc8be
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-09 12:51:05 +0000

    Deploying to gh-pages from @ Vozniak-Ihor/goit-js-hw-09@9989140250218ee64ee3d5597d92824f7648c2f9 🚀

---
+++ 01-color-switcher.51939137.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(){changeColor=setInterval((()=>{t.disabled=!0,o.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(changeColor),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.51939137.js.map
---
+++ 01-color-switcher.a83d1c17.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){changeColor=setInterval((function(){t.disabled=!0,n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(changeColor),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.a83d1c17.js.map
