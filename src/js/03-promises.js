ef9fd7ca4f09fc51ee2141b6b0ed7413d37bfafd
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-11 23:50:42 +0300

    Task_3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', handleFormElSubmit);
+
+async function handleFormElSubmit(event) {
+  event.preventDefault();
+  const inputDelayValue = Number(event.target.elements.delay.value);
+  const inputStepValue = Number(event.target.elements.step.value);
+  const inputAmountValue = Number(event.target.elements.amount.value);
+
+  for (let i = 1; i <= inputAmountValue; i += 1) {
+    let currentDelay = inputDelayValue + (i - 1) * inputStepValue;
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        window.alert(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        window.alert(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
